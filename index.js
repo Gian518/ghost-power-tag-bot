044d01e04a8f9b4299cc879923aca032613502f9
Gianluca Iuliano <gian518@me.com>
2021-04-03 15:12:05 +0200

    Added /all, /me, merged /commands with /start

--- index.js
+++ index.js
@@ -15,12 +15,14 @@ var tags = require('./tags.json')
 var token = process.env.TELEGRAM_TOKEN
 var bot = new TelegramBot(token, { polling: true })
 
-// Start the bot
-bot.onText(/\/start/, (msg, match) => {
+let welcomeText = "Ciao! GhostPowerTag ti consente di salvare il tuo tag e mostrarlo agli altri giocatori. I comandi sono:\n/tag `<iltuotag>` - Aggiunge il tuo tag alla collezione o modifica quello esistente\n/user `<nickname>` - Mostra il tag di un utente\n/all - Mostra tutti i tag\n/me - Mostra il tuo tag\n/commands - Mostra questo messaggio\n\nBot creato da @Gian518"
+
+// Start the bot with a welcome message, it does the same with /commands
+bot.onText(/\/start|\/commands/, (msg, match) => {
     let chatID = msg.chat.id
     try {
-        let response = "Ciao! CODBot ti consente di salvare il tuo tag e mostrarlo agli altri giocatori. I comandi sono:\n/tag <iltuotag> - Aggiunge il tuo tag alla collezione\n/user <nickname> - Mostra il tag di un utente\n/comandi - Mostra questo messaggio\n\nBot creato da @Gian518"
-        bot.sendMessage(chatID, response)
+        let response = welcomeText
+        bot.sendMessage(chatID, response, { parse_mode: 'Markdown' })
     } catch (error) {
         let response = "Si è verificagto un errore."
         console.log("Error:", error)
@@ -28,20 +30,7 @@ bot.onText(/\/start/, (msg, match) => {
     }
 })
 
-// Same as /start
-bot.onText(/\/comandi/, (msg, match) => {
-    let chatID = msg.chat.id
-    try {
-        let response = "Ciao! CODBot ti consente di salvare il tuo tag e mostrarlo agli altri giocatori. I comandi sono:\n/tag <iltuotag> - Aggiunge il tuo tag alla collezione\n/user <nickname> - Mostra il tag di un utente\n/comandi - Mostra questo messaggio\n\nBot creato da @Gian518"
-        bot.sendMessage(chatID, response)
-    } catch (error) {
-        let response = "Si è verificagto un errore."
-        console.log("Error:", error)
-        bot.sendMessage(chatID, response)
-    }
-})
-
-// Return the searched tag
+// Return the searched user tag
 bot.onText(/\/user (.+)/, (msg, match) => {
     let chatId = msg.chat.id
     try {
@@ -77,9 +66,50 @@ bot.onText(/\/tag (.+)/, (msg, match) => {
             } else {
                 response = 'Tag salvato!'
                 bot.sendMessage(chatId, response)
+                tags = require('./tags.json')
             }
         })
     } catch (error) {
         bot.sendMessage(chatId, 'Si è verificato un errore, riprova.')
+    }
+})
+
+// Show all tags
+bot.onText(/\/all/, (msg, match) => {
+    let chatId = msg.chat.id
+    try {
+        let response = ""
+        if(Object.entries(tags).length > 0){
+            response = "Ecco tutti i tag:"
+            Object.entries(tags).forEach(tag => {
+                response += "\n@" + tag[0] + ": #" + tag[1]
+            })
+        } else {
+            response = "Non ci sono ancora tag. Riprova più tardi!"
+        }
+        bot.sendMessage(chatId, response)
+    } catch (error) {
+        let response = 'Si è verificato un errore. Riprova.'
+        console.log("Error in /all: ", error)
+        bot.sendMessage(chatId, response)
+    }
+})
+
+// Show user tag
+bot.onText(/\/me/, (msg, match) => {
+    let chatId = msg.chat.id
+    try {
+        let response = ''
+        let tag = tags[msg.chat.username.toLowerCase()]
+        if(tag){
+            response = 'Il tuo tag è #' + tag + '\nPuoi reimpostarlo con il comando /tag `<iltuotag>`'
+        } else {
+            response = 'Non hai ancora salvato il tuo tag. Invialo con il comando /tag `<iltuotag>`'
+        }
+        bot.sendMessage(chatId, response, { parse_mode: 'Markdown' })
+    } catch (error) {
+        console.log("Error in /me", error)
+        let response = "Si è verificato un errore. Riprova."
+        bot.sendMessage(chatId, response)
     }
 })
